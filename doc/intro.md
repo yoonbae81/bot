e07335c19ae1d9e29e85a6fbb78fc4a6a821bd3b
Yoonbae Cho <y@xcv.kr>
2018-02-15 11:47:15 +0900

    removed due to name change

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to pricemonitor
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- src/pricemonitor/core.clj
+++
@@ -1,9 +0,0 @@
-(ns pricemonitor.core
-  (:gen-class))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
-
-(+ 1 1)
